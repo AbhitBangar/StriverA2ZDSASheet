d4a59b78000bb89c78a0ea98f385e7a96b76b35c
SwarajShelavale <shelavaleswaraj1610@gmail.com>
2024-11-20 02:05:56 +0530

    linear search program is easy in java:)

--- src/Arrays/Easy/Linear_Search.java
+++ src/Arrays/Easy/Linear_Search.java
@@ -2,6 +2,16 @@ package Arrays.Easy;
 
 public class Linear_Search {
     public static void main(String[] args) {
-
+        int[] arr = {2, 34, 5, 6, 3, 33};
+        int number = 5, index = -1;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == number)
+                index = i;
+            break; // --> no need to keep traverse rest of the array
+        }
+        System.out.println(index);
     }
 }
+
+//TC = O(N;
+//SC = O(1);
