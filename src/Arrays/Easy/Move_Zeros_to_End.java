67a02d33e7b001826af4dbf4adc74d77a58e5154
SwarajShelavale <shelavaleswaraj1610@gmail.com>
2024-11-20 01:42:25 +0530

    paper to ide

--- src/Arrays/Easy/Move_Zeros_to_End.java
+++ src/Arrays/Easy/Move_Zeros_to_End.java
@@ -2,5 +2,37 @@ package Arrays.Easy;
 
 public class Move_Zeros_to_End {
     public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int n = 10;
+        int[] ans = moveZerosToEnd(n, arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public static int[] moveZerosToEnd(int n, int []a) {
+        int j = -1;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return a;
+
+        for (int i = j + 1; i < n; i++) {
+            if (a[i] != 0) {
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                j++;
+            }
+        }
+        return a;
     }
 }
+
+// TC = O(N)
+// SC = O(1)
